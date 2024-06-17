f8cd3aa00f906c98eb4e4dcd41450d8ae14cf8af
Anton Hryshchuk <grutoha@gmail.com>
2024-06-18 00:59:56 +0200

    123123

--- src/library/firebase.js
+++ src/library/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: 'react-online-chat-dfdd4.firebaseapp.com',
-    projectId: 'react-online-chat-dfdd4',
-    storageBucket: 'react-online-chat-dfdd4.appspot.com',
-    messagingSenderId: '633081277229',
-    appId: '1:633081277229:web:688f439a2642c9d4c2c1a4',
-    databaseURL: 'https://react-online-chat-dfdd4-default-rtdb.firebaseio.com',
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    databaseURL: import.meta.env.VITE_DATABASE_URL,
 }
 
 const app = initializeApp(firebaseConfig)
